aa6c5ef48024a27f5c00d3cf89612107abdabf43
RohitRox <link2j.roy@gmail.com>
2012-03-03 22:32:47 +0545

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Createimpress::Application.config.secret_token = '5a43f933765c07414ee04e41c43ae1e23d100ccd73cf5d10cee45da3677016123a872943e5916129a43df5971f357ff1a893d1ce62e71f469b8824174cbf03fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Createimpress::Application.config.session_store :cookie_store, key: '_createimpress_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Createimpress::Application.config.session_store :active_record_store
